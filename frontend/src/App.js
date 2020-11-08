c8aba68efdb2e85035981e5424aeda8eb6afb722
overrkill <zabhishekkale@gmail.com>
2020-11-08 20:24:52 +0530

    updated Donate and details

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
-import './App.css';
+
+import {useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { AppBar , Toolbar,IconButton,Typography,Grid,Card,CardActions,CardContent } from '@material-ui/core';
+import { AppBar , Toolbar,IconButton,Typography,Grid,Card,CardActions,CardContent,ButtonGroup,Drawer,List,ListItem,ListItemIcon,ListItemText } from '@material-ui/core';
 import  MenuIcon  from '@material-ui/icons/Menu';
 import Button from '@material-ui/core/Button';
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -39,7 +41,8 @@ function App() {
       <AppBar position="static">
   <Toolbar>
     <IconButton edge="start" classname={classes.menuButton}  color="inherit" aria-label="menu">
-      <MenuIcon />
+      <MenuIcon   />
+      
     </IconButton>
     <Typography variant="h6" className={classes.title} >
       We Stand Together
@@ -55,7 +58,7 @@ function App() {
               <Card className={classes.card}>
       <CardContent>
         <Typography className={classes.cardtitle} color="textSecondary" gutterBottom>
-        I have requirement for 
+        We have requirement for 
         </Typography>
         <Typography variant="h5" component="h2">
          {bull}  requiredItem {value}
@@ -68,7 +71,10 @@ function App() {
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small" >Donate</Button>
+      <ButtonGroup color="primary" aria-label="outlined primary button group">
+  <Button>Details</Button>
+  <Button>Donate</Button>
+</ButtonGroup>
       </CardActions>
     </Card>
             </Grid>

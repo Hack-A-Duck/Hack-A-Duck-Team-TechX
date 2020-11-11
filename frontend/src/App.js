225316adf567aed16770c66ecf353193aeb4bb4c
overrkill <zabhishekkale@gmail.com>
2020-11-11 09:50:55 +0530

    added routing - / , /login ,/signup(added page)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,22 @@
 
 import Home from './components/Home/Home';
 import Login from './components/login/login';
+import SignUp from './components/signup/signup';
+import {BrowserRouter as Router , Switch ,Route} from 'react-router-dom';
+
+
 
 function App() {
  
   return (
     
-    <div >
-      <Login/>
-      <Home/>
-    </div>
+   <Router>
+     <Switch>
+       <Route exact path='/' component={Home} />
+       <Route exact path='/login' component={Login}/>
+       <Route exact path='/signup' component={SignUp}/>
+     </Switch>
+   </Router>
   );
 }
 
